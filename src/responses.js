db31ec83d0e077f215373797d671bb7ed722d14c
Audrey Main <aam6039@rit.edu>
2023-03-01 16:33:21 -0500

    added stuff todo

---
+++ src/responses.js
@@ -0,0 +1,13 @@
+const fs = require('fs'); // pull in the file system module
+
+const basicDataJSON = fs.readFileSync(`${__dirname}/../client/data/pokemon-basic-data.json`);
+
+const getBasicData = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.write(basicDataJSON);
+  response.end();
+};
+
+module.exports = {
+  getBasicData
+};
